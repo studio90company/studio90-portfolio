110724b8711103c8b6f004822c4dc5ae80d9ca25
philomenambura <philomenambura@gmail.com>
2020-04-18 21:39:42 +0300

    edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,7 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
+       
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
@@ -41,19 +35,10 @@ const Footer = class extends React.Component {
                     </li>
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                        Join Us
                       </Link>
                     </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
+                    
                   </ul>
                 </section>
               </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ const Navbar = class extends React.Component {
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Join Us
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
